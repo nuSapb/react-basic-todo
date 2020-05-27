76c8193d54ee05b3bfb8e6fc3ab39e6d32f9841e
Anu Sakpibal <Anu.Sakpibal@kimballelectronics.com>
2020-05-27 17:14:29 +0700

    Updates

---
+++ precache-manifest.4a27b2eeeb54bbe08f1eb7286db5366b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "645220d5834ad36de691460dd05a14ba",
+    "url": "/react-basic-todo/index.html"
+  },
+  {
+    "revision": "2803a8d165d20c13a331",
+    "url": "/react-basic-todo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f8e14a192ccef43cf03a",
+    "url": "/react-basic-todo/static/js/2.dd0320b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-basic-todo/static/js/2.dd0320b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2803a8d165d20c13a331",
+    "url": "/react-basic-todo/static/js/main.a3efe6c6.chunk.js"
+  },
+  {
+    "revision": "be5173e205ea093e9754",
+    "url": "/react-basic-todo/static/js/runtime-main.cd94f286.js"
+  }
+]);
